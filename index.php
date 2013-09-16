f242d11087c910378eb221f3e32c0011acd1a6c7
Ivan Sinyagin <isinyagin@fas.harvard.edu>
2013-09-16 01:16:58 -0400

    Added mini-bio, editor preferences and bootstrap boilerplate

--- index.php
+++ index.php
@@ -1,12 +1,35 @@
 <!DOCTYPE HTML>
 <html>
+  <head>
+    <title>CS15 Project 1</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
 
-<head>
-    <title> CSCI 15 Project 1 </title>
-</head>
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <h1>Hello, CS15!</h1>
 
-<body>
-<?php echo "CSCI 15 Project 1" ?>
-</body>
+    <h2> 
+        My name is <a href = "mailto:isinyagin@fas.harvard.edu">Ivan Sinyagin</a> 
+    </h2>
+    
+    <p> I'm a senior computer science concentrator at Harvard University. </p>
+    
+    <p> As for my editors/IDE choice, I'm old-fashioned; I use vim and
+        occasionally emacs for almost all my workflow. The only exceptions are
+        iOS applications, which require Xcode and sophisticated unit testing in
+        java apps. For this course, I hope to use only command line tools
+    </p>
 
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="//code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
 </html>
