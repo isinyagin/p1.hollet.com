c2839bb5fdfea5de806296bd74b3b01bae4f78a9
Ivan Sinyagin <isinyagin@fas.harvard.edu>
2013-09-17 00:41:58 -0400

    Introduced a grid

--- index.php
+++ index.php
@@ -1,38 +1,42 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Bootstrap 101 Template</title>
+    <title>CS15 Project 1 -- Info</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
     <link href="_/css/hollet_styles.css" rel="stylesheet" media="screen">
   </head>
-  <body>
-    <div class="container">
-      <div class="sub_container">
-        <h1>Hello, CS15!</h1>
 
-        <h2> 
-          My name is <a href = "mailto:isinyagin@fas.harvard.edu">Ivan Sinyagin</a> 
-        </h2>
+  <body id="home">
+    <section class="container">
+      <h1>Hello, CS15!</h1>
+      <div class="content row">
+        <section class="picture col col-lg-4">
+          <img src="https://dl.dropboxusercontent.com/u/5983981/boston_commons.jpg" 
+               alt="It's me">
+        </section><!-- picture -->
 
-        <img src="https://dl.dropboxusercontent.com/u/5983981/boston_commons.jpg" 
-             alt="It's me">
+        <section class="main col col-lg-8">
+          <h2> 
+            My name is <a href = "mailto:isinyagin@fas.harvard.edu">Ivan Sinyagin</a> 
+          </h2>
+          <p class="lead"> 
+            I'm a senior computer science concentrator at Harvard University.
+            Most of my experience comes from back-end and low-level programming 
+            (networks, compilers) or iOS development. 
+            Have never done any front-end web things, hence will be catching up.
+          </p>
 
-        <p class="lead"> 
-          I'm a senior computer science concentrator at Harvard University.
-          Most of my experience comes from back-end and low-level programming 
-          (networks, compilers) or iOS development. 
-          Have never done any front-end web things, hence will be catching up.
-        </p>
+          <p class="lead"> 
+            As for my editors/IDE choice, I'm old-fashioned; I use vim and
+            occasionally emacs for almost all my workflow. The only exceptions are
+            iOS applications, which require Xcode and sophisticated unit testing in
+            java apps. For this course, I hope to use only command line tools
+          </p>
+        </section><!-- main -->
 
-        <p class="lead"> 
-          As for my editors/IDE choice, I'm old-fashioned; I use vim and
-          occasionally emacs for almost all my workflow. The only exceptions are
-          iOS applications, which require Xcode and sophisticated unit testing in
-          java apps. For this course, I hope to use only command line tools
-        </p>
-      </div>
-    </div>
+      </div><!-- content -->
+    </section><!-- container -->
 
     <script src="_/js/bootstrap.js"></script>
     <script src="_/js/hollet_script.js"></script>
